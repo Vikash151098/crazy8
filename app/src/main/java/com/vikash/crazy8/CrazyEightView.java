0b692d678c11166a92649b37742676f229bab069
Vikash151098 <vkumar0961@gmail.com>
2019-01-25 20:10:48 +0530

    crazy8

---
+++ app/src/main/java/com/vikash/crazy8/CrazyEightView.java
@@ -0,0 +1,53 @@
+package com.vikash.crazy8;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+public class CrazyEightView extends View {
+
+        private Paint redPaint;
+        private int circleX;
+        private int circleY;
+        private float radius;
+
+        public CrazyEightView(Context context) {
+            super(context);
+            redPaint = new Paint();
+            redPaint.setAntiAlias(true);
+            redPaint.setColor(Color.RED);
+            circleX = 100;
+            circleY = 100;
+            radius = 30;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawCircle(circleX, circleY, radius, redPaint);
+        }
+
+        public boolean onTouchEvent(MotionEvent event) {
+            int eventaction = event.getAction();
+            int X = (int)event.getX();
+            int Y = (int)event.getY();
+
+            switch (eventaction ) {
+
+                case MotionEvent.ACTION_DOWN:
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    circleX = X;
+                    circleY = Y;
+                    break;
+            }
+            invalidate();
+            return true;
+
+        }
+
+    }
---
+++ app/src/main/java/com/vikash/crazy8/MainActivity.java
@@ -0,0 +1,27 @@
+package com.vikash.crazy8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TitleView tView = new TitleView(this);
+
+        tView.setKeepScreenOn(true);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(tView);
+
+
+    }
+}
